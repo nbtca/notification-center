cf664b1f09ea1b08092733b999bed445cf3dab02
Mephisto <1443735959@qq.com>
2023-11-18 17:11:50 +0800

    init

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Data string `json:"data"`
+	// To be implemented
+}
+
+var (
+	clients   = make(map[*websocket.Conn]bool)
+	clientsMu sync.Mutex
+	upgrader  = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+)
+
+func handleWsMessage(message []byte) {
+}
+
+func ws(c *gin.Context) {
+	go func() {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			log.Println(err)
+			return
+		}
+		clientsMu.Lock()
+		clients[conn] = true
+		clientsMu.Unlock()
+		defer conn.Close()
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				break
+			}
+			handleWsMessage(message)
+		}
+	}()
+}
+
+func webhook(c *gin.Context) {
+	var msg Message
+	c.ShouldBind(&msg)
+	if err := c.ShouldBindJSON(&msg); err != nil {
+		log.Println(err)
+		return
+	}
+	jsonData, err := json.Marshal(msg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	clientsMu.Lock()
+	defer clientsMu.Unlock()
+
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, jsonData)
+		if err != nil {
+			log.Println("Failed to send WebSocket message:", err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
+
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "test passed")
+	})
+
+	r.POST("/webhook", webhook)
+	r.GET("/ws", ws)
+
+	r.Run(":8000")
+}
