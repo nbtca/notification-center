df461f761a65b102bfc89fb4290ba0e255f55a77
LazuliKao <LazuliKao233@outlook.com>
2023-11-18 17:38:32 +0800

    optimize

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,8 +12,8 @@ import (
 )
 
 var (
-	clients   = make(map[*websocket.Conn]bool)
-	clientsMu sync.Mutex
+	clients   = make(map[*websocket.Conn]bool) //已经链接的ws客户端
+	clientsMu sync.Mutex                       //客户端锁
 	upgrader  = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
@@ -22,10 +21,12 @@ var (
 	}
 )
 
+// 从客户端收到消息
 func handleWsMessage(message []byte) {
 }
 
-func ws(c *gin.Context) {
+// 处理ws路径的请求
+func handleWs(c *gin.Context) {
 	go func() {
 		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
@@ -48,22 +49,23 @@ func ws(c *gin.Context) {
 	}()
 }
 
-func webhook(c *gin.Context) {
+// 处理webhook请求(POST)
+func handleWebhook(c *gin.Context) {
 	go func() {
-		var msg map[string]interface{}
-		if err := c.ShouldBindJSON(&msg); err != nil {
-			log.Println(err)
-			return
-		}
-		jsonData, err := json.Marshal(msg)
+		// var msg map[string]interface{}
+		// if err := c.ShouldBindJSON(&msg); err != nil {
+		// 	log.Println(err)
+		// 	return
+		// }
+		// jsonData, err := json.Marshal(msg)
+		jsonData, err := c.GetRawData()
 		if err != nil {
 			log.Println(err)
 			return
 		}
-
+		fmt.Println(string(jsonData))
 		clientsMu.Lock()
 		defer clientsMu.Unlock()
-
 		for client := range clients {
 			err := client.WriteMessage(websocket.TextMessage, jsonData)
 			if err != nil {
@@ -74,17 +76,13 @@ func webhook(c *gin.Context) {
 		}
 	}()
 }
-
 func main() {
 	r := gin.Default()
-	r.Use(cors.Default())
-
-	r.GET("/", func(c *gin.Context) {
+	r.Use(cors.Default())             //跨域
+	r.GET("/", func(c *gin.Context) { //测试
 		c.String(http.StatusOK, "test passed")
 	})
-
-	r.POST("/webhook", webhook)
-	r.GET("/ws", ws)
-
-	r.Run(":8000")
+	r.POST("/webhook", handleWebhook) //webhook服务
+	r.GET("/ws", handleWs)            //ws服务
+	r.Run(":8000")                    //启动服务
 }
